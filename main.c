a3d0e4f44edd929f2df977a4e6b62b1f0124c20f
Irina Kalinina <irkalini@bess-f3r4s1.clusters.42paris.fr>
2025-02-21 16:45:27 +0100

    g0

--- main.c
+++ main.c
@@ -3,14 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: enrmarti <enrmarti@student.42.fr>          +#+  +:+       +#+        */
+/*   By: irkalini <irkalini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 17:54:30 by enrmarti          #+#    #+#             */
-/*   Updated: 2025/02/19 19:03:07 by enrmarti         ###   ########.fr       */
+/*   Updated: 2025/02/21 16:09:04 by irkalini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <readline/readline.h>
 #include <readline/history.h>
@@ -23,23 +24,24 @@ int	main(void)
 {
 	char	**output;
 	char	*rl;
-	int i;
+	int		i;
+	int		pid;
 
 	while (1)
 	{
 		i = 0;
-		readline("minishell>");
-		add_history(rl_line_buffer);
-		if (strcmp(rl_line_buffer, "q") == 0)
+		rl = readline("minishell>");
+		add_history(rl);
+		if (strcmp(rl, "q") == 0)
 		{
 			rl_clear_history();
-			free(rl_line_buffer);
+			free(rl);
 			return (0);
 		}
-		output = ft_split(rl_line_buffer, '|');
-		while (output[i])	
+		
+		while (output[i])
 			printf("%s\n", output[i++]);
-		// rl_redisplay();
+
 	}
 	return (0);
 }
