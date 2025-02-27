1152bdb0afba2386dafb33bf33a75c43878379b6
Irina Kalinina <irakalina2004@gmail.com>
2025-02-28 00:12:43 +0100

    env and pwd done

---
+++ srcs/builtins/pwd.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pwd.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: irkalini <irkalini@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/27 22:55:41 by irkalini          #+#    #+#             */
+/*   Updated: 2025/02/27 23:35:46 by irkalini         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+void	pwd_cmd(void)
+{
+	char *buf;
+
+	buf = malloc(sizeof(char) * 1024);
+	if (!buf)
+		return ;
+	if (!getcwd(buf, 1024))
+	{
+		free(buf);
+		return ;
+	}
+	printf("%s\n", buf);
+	free(buf);
+}
