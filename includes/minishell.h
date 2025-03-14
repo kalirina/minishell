bd6f288c3303c0f4041702e96eed509ba871ef35
Irina Kalinina <irakalina2004@gmail.com>
2025-03-14 13:11:48 +0100

    merged header file

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,47 +3,77 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: irkalini <irkalini@student.42.fr>          +#+  +:+       +#+        */
+/*   By: enrmarti <enrmarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/21 13:55:50 by enrmarti          #+#    #+#             */
-/*   Updated: 2025/02/23 22:29:02 by irkalini         ###   ########.fr       */
+/*   Updated: 2025/03/11 15:47:40 by enrmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
-# define MINISHELL_H
+#define MINISHELL_H
 
-# include <stdlib.h>
-# include <stdio.h>
-# include <unistd.h>
-# include <sys/stat.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <stdbool.h>
+#include "../libft/libft.h"
 
+# define MAX_TOKEN 100
 
-typedef struct  s_command
+# define CMD 1			// commands and args
+//# define VAR 2			// $var
+# define PIPE 3			// |
+# define INPUT 4		// <
+# define OUTPUT 5		// >
+# define APPEND 6		// >>
+# define HEREDOC 7		// <<
+//# define SEMICOLON 8	// ;
+//# define S_QUOTE 9		// '
+//# define D_QUOTE 10		// "
+ 
+// typedef enum e_node
+// {
+// 	WORD,
+// 	PIPE,			//|
+// 	OUT, 			//>
+// 	D_OUT,	 		//>>
+// 	IN,				//<
+// 	HEREDOC, 		//<<
+// } t_node_type;
+
+typedef struct	s_token
 {
-    char    *command;
-    char    **args;
-    char    *opt;
-} t_command;
+	int				index;
+	char			*str;
+	struct s_token	*next;
+} t_token;
 
-typedef enum    s_types
+typedef struct	s_node
 {
-    PIPE,
-    IN,
-    OUT,
-    HEREDOC,
-    OUT_APPEND
-} t_types;
+	int				type;
+	char			**args;
+	struct s_node	*left;
+	struct s_node	*right;
+} t_node;
 
-typedef struct  s_separator
+typedef struct	s_shell
 {
-    t_types     type;
-    t_command   *left;
-    t_command   *right;
-} t_separator;
+	t_token		*tokens;
+	t_node		*ast;
+} t_shell;
 
-// builtins
-int		cd_cmd(char *path);
-char	*join_components(char **components);
+void	lexer(t_shell *shell, char *line);
+void	parser(t_shell *shell);
+
+//parsing utils
+char	*skip_spaces(char *str);
+bool	is_space(char str);
+char	*ft_strndup(const char *s, size_t n);
+
+t_token	*add_token(t_token **head, t_token *new);
+
+
 
 #endif
--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -1,29 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pwd.c                                              :+:      :+:    :+:   */
+/*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: irkalini <irkalini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/27 22:55:41 by irkalini          #+#    #+#             */
-/*   Updated: 2025/03/14 12:20:23 by irkalini         ###   ########.fr       */
+/*   Created: 2025/03/14 12:20:42 by irkalini          #+#    #+#             */
+/*   Updated: 2025/03/14 12:48:04 by irkalini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+#include "../libft/libft.h"
 
-void	pwd_cmd(void)
+void echo_cmd(int argc, char **argv)
 {
-	char *buf;
+	int	i;
 
-	buf = malloc(sizeof(char) * 1024);
-	if (!buf)
+	if (argc == 0)
 		return ;
-	if (!getcwd(buf, 1024))
+	i = 0;
+	if (ft_strncmp(argv[0],"-n",2) == 0)
+		i++;
+	while (argv[i])
 	{
-		free(buf);
-		return ;
+		printf("%s", argv[i]);
+		printf(" ");
+		i++;
 	}
-	printf("%s\n", buf);
-	free(buf);
+	if (ft_strncmp(argv[0],"-n",2) != 0)
+		printf("\n");
+}
+
+int main(int argc, char **argv){
+	echo_cmd(argc - 1, argv + 1);
+	return (0);
 }
